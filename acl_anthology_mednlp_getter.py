91b30c63bacd8a9583a6a1bc3d856c9b263fa707
iBotamon <ibotamon@gmail.com>
2019-06-29 08:49:02 +0900

    Update acl_anthology_mednlp_getter.py
    implemented new queries

--- acl_anthology_mednlp_getter.py
+++ acl_anthology_mednlp_getter.py
@@ -38,7 +38,7 @@ def mednlp_parse(res, verbose=True):
 	soup = bs4.BeautifulSoup(html, 'html5lib')
 
 	# query
-	queries = ['medic', 'biomedic', 'health', 'clinic', 'life', 'care', 'pharm', 'drug', 'surg', 'emergency', 'ICU', 'hospital', 'patient', 'doctor', 'disease', 'illness', 'symptom', 'psychol', 'psychiat', 'mental', 'radiol', 'x-ray', 'report', 'CT', 'MRI', 'radiograph', 'tomograph', 'magnetic']
+	queries = ['medic', 'biomedic', 'health', 'clinic', 'life', 'care', 'pharm', 'drug', 'surg', 'emergency', 'ICU', 'hospital', 'patient', 'doctor', 'disease', 'illness', 'symptom', 'treatment', 'cancer', 'psycholog', 'psychiat', 'mental', 'radiol', 'x-ray', 'report', 'CT', 'MRI', 'radiograph', 'tomograph', 'magnetic']
 
 	result = []
 	prev_title = ''
